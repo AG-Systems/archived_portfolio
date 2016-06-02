dd4f869c5d90b95f22504e0b004618ef94363728
Auriga <AG-Systems@users.noreply.github.com>
2016-06-02 03:52:02 -0700

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -12,33 +12,34 @@ angular
     function buildGridModel(tileTmpl){
       var it, results = [ ];
 
-      for (var j=0; j<11; j++)
+      for (var j=11; j>0; j--)
       {
 
         it = angular.extend({},tileTmpl);
-        it.icon  = it.icon + (j+1);
-        it.title = it.title + (j+1);
+        it.icon  = it.icon + (j-1);
+        it.title = it.title + (j-1);
         it.span  = { row : 1, col : 1 };
 
-        switch(j+1)
+        switch(j-1)
         {
+          case 0:
+            it.title = "My First Video";
+            break;
           case 1:
             it.background = "red";
+            it.title = "Yo";
             // it.span.row = it.span.col = 2;
             break;
-
           case 2: it.background = "green";         break;
           case 3: it.background = "darkBlue";      break;
           case 4:
             it.background = "blue";
             // it.span.col = 2;
             break;
-
           case 5:
             it.background = "yellow";
             //it.span.row = it.span.col = 2;
             break;
-
           case 6: it.background = "pink";          break;
           case 7: it.background = "darkBlue";      break;
           case 8: it.background = "purple";        break;
