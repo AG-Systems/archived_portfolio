d1195b7110e937fc7d81b2c4c714e96035c2cc5a
Auriga <AG-Systems@users.noreply.github.com>
2016-06-06 00:04:24 -0700

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,77 @@
+var app = angular.module('demo', ['ngFlowGrid']);
+
+app.controller('appCtrl',['$scope','fgDelegate',function($scope,fgDelegate){
+	$scope.items = [
+		{
+			id:1,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Why assumptions are wrong',
+			link:'https://www.google.com/',
+		},
+		{
+			id:2,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+		{
+			id:3,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+		{
+			id:4,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+		{
+			id:5,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+		{
+			id:6,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+		{
+			id:7,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+		{
+			id:8,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+		{
+			id:9,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+		{
+			id:10,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+		{
+			id:11,
+			img:'http://placehold.it/400x500/D25064/fff',
+			name:'Lorem ipsum dolor sit amet',
+			link:'https://www.google.com/',
+		},
+
+	]
+    // then you can:
+    // homePageGrid.minItemWidth = 150;
+    // homePageGrid.refill();
+
+}]);
